f63523c0056b855144fbd314c8d97694e10854bf
Scoti Dodson <scotidodson@gmail.com>
2018-09-09 14:38:01 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -58,3 +58,5 @@ def shortened_tweet_truncator(tweet)
     tweet
   end 
 end 
+
+shortened_tweet_truncator("GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!")
